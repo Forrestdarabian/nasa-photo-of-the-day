2c304ad91a17604035c87ce590c0d9695ae72bff
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-07-18 12:12:14 -0700

    MVP FINALLY

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import PhotoCard from "./components/Grid.js"
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    console.log("first render")
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=3e36NVbhT9wtjMkpVR6BgnN1XJbYgvoGi4ffuFCf')
+  .then(res => {
+    console.log(res.data);
+    setData(res.data);
+  });
+  }, [])
+
+ 
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        Hello! Here, you can find a beautiful image from the NASA Website!
       </p>
+      <PhotoCard title={data.title}
+      url={data.url}
+      explanation={data.url}/>
     </div>
   );
 }
